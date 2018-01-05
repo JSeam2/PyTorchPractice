1e71459c4491b01206b291dbd4f79166d6aae15b
JSeam <joshiaseam@gmail.com>
2018-01-05 19:37:52 +0800

    completed cifar tutorial

--- CIFAR/train.py
+++ CIFAR/train.py
@@ -40,10 +40,11 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 
 def imshow(img):
     img = img /2 + 0.5 # unnormalize the image
-    npimg = img.numpy
+    npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
 
-
+"""
 # get some random training images
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
@@ -51,8 +52,120 @@ images, labels = dataiter.next()
 imshow(torchvision.utils.make_grid(images))
 
 # print labels
-print(" ".join('%5x' % classes[labels[j]] for j in range(4)))
+print(" ".join('%5s' % classes[labels[j]] for j in range(4)))
+"""
 
 
 
+# Define the convolutional neural network
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5) # 3 for rgb
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+
+# Define a loss function
+import torch.optim as optim
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+# Train the network
+for epoch in range(1):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the input 
+        inputs, labels = data
+
+        # wrap them in Variable
+        inputs, labels = Variable(inputs), Variable(labels)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward --> backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print the stats
+        running_loss += loss.data[0]
+        # only print every 2000 mini-batches
+        if i % 2000 == 1999:
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1,
+                                            running_loss/2000))
+            running_loss = 0.0
+
+print("Finish Training")
+
+
+# work on the test images
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+# print images 
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+outputs = net(Variable(images))
+
+_, pred = torch.max(outputs.data, 1)
+print("Predicted: ", " ".join('%5s' % classes[pred[j]] for j in range(4)))
+
+correct = 0
+total = 0
+for data in testloader:
+    images, labels = data
+    outputs = net(Variable(images))
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+for data in testloader:
+    images, labels = data
+    outputs = net(Variable(images))
+    _, predicted = torch.max(outputs.data, 1)
+    c = (predicted == labels).squeeze()
+    for i in range(4):
+        label = labels[i]
+        class_correct[label] += c[i]
+        class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
+
+
+# to train in gpu 
+# cuda()
+# net.cuda()
+# inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
